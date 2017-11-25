b5bf8f1d7f950d803f9410c18277c4fd9caca9a8
AGCB <oooalexoooo@gmail.com>
2017-11-25 11:45:44 -0800

    Create 6.js

---
+++ 6.js
@@ -0,0 +1,65 @@
+/* 
+  This is the 3 boxes layed out horizontally filling the whole screen.
+  It is responsive, but flexbox wasn't needed. {height: #vh;} works great.
+  I did notice that it helped solidify things by putting temporary borders on everything
+  {border: 10px dotted red;}
+*/
+
+//Here is the div Soup
+/* 
+return (
+     
+      <div id="myWrapper">
+     <div id="myDiv">
+     
+     </div>
+
+     <div id="myDiv2">
+     
+     </div>
+
+     <div id="myDiv3">
+  
+     </div>
+
+     </div>
+    );
+*/
+
+//Here is the CSS for App.css...
+
+/* 
+#myWrapper {
+  border: 5px dotted green;
+  height: 100vh;
+}
+
+#myDiv {
+  background-color: red;
+  width: 100%;
+  border: 5px dotted black;
+  height: 30vh;
+
+}
+
+#myDiv2 {
+  background-color: yellow;
+   border: 5px dotted black;
+   height: 30vh;
+ 
+
+}
+
+#myDiv3 {
+  background-color: blue;
+  border: 5px dotted black;
+  height: 39vh;
+  
+
+}
+
+body {
+  height: 100vh;
+  
+}
+*/
